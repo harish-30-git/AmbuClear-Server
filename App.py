b218897013b287eab5276d71f9ba161e44f45064
harish-30-git <srihariprakash1112@gmail.com>
2025-04-12 18:16:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,8 +7,21 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# Load Firebase credentials
-cred = credentials.Certificate("path/to/your-service-account.json")
+service_account_dict = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": "googleapis.com"
+}
+
+cred = credentials.Certificate(service_account_dict)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ambuclear-ee248-default-rtdb.firebaseio.com/' 
 })
