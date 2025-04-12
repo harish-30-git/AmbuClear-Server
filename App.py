078a0f964ff109ee31be5c792777565888288c72
harish-30-git <srihariprakash1112@gmail.com>
2025-04-12 18:20:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -58,4 +58,5 @@ def location():
         return jsonify({"status": "failure", "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
