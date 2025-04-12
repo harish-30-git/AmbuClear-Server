120c8e1f1a5ef8081b2c861da6c6f704f6a82e08
harish-30-git <srihariprakash1112@gmail.com>
2025-04-12 22:20:38 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,37 +1,49 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import firebase_admin
-from firebase_admin import credentials, db, initialize_app
-import os, json, base64
+from firebase_admin import credentials, db
+import os
+import base64
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Read env variable (Render or .env file)
-encoded = os.getenv("FIREBASE_CREDENTIALS_B64")
-decoded = base64.b64decode(encoded).decode("utf-8")
-firebase_config = json.loads(decoded)
-firebase_url=os.getenv("FIREBASE_URL")
-cred = credentials.Certificate(firebase_config)
+firebase_creds_b64 = os.getenv("FIREBASE_CONFIG_BASE64")
+if not firebase_creds_b64:
+    raise ValueError("Missing FIREBASE_CONFIG_BASE64 in .env")
+
+
+decoded_creds = base64.b64decode(firebase_creds_b64).decode('utf-8')
+firebase_creds = json.loads(decoded_creds)
+
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
-    'databaseURL':firebase_url
+    'databaseURL': os.getenv("FIREBASE_DB_URL")
 })
 
 @app.route('/location', methods=['POST'])
 def location():
     try:
+        
         data = request.get_json(force=True)
         print(f"Received location data: {data}")
 
+        
         esp32_id = data.get('esp32_id')
         name = data.get('name')
         lat = data.get('latitude') 
         lon = data.get('longitude') 
         status = data.get('status')  
 
+    
         if not all([esp32_id, name, lat, lon, status]):
             return jsonify({"status": "failure", "error": "Missing required fields"}), 400
 
+        
         ref = db.reference(f'/locations/{name}/{esp32_id}')
         ref.set({
             'status': status
@@ -45,8 +57,10 @@ def location():
             "longitude": lon,
             "status": status
         })
+    
     except Exception as e:
         return jsonify({"status": "failure", "error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
